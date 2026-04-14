47d1373e95a30f3e2b5062ae501f976d64786f82
Hrinchenko <digdog649@gmail.com>
2026-04-14 12:29:28 +0200

    initial dev

--- src/main/java/ua/hrinchenko/Main.java
+++ src/main/java/ua/hrinchenko/Main.java
@@ -2,6 +2,6 @@ package ua.hrinchenko;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hrinchenko-Island");
+        System.out.println("Hrinchenko-Island_dev");
     }
 }

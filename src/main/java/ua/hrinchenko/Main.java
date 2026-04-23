e90249c110cb3e81a2cfaf522fca81c0f8efbfbc
Hrinchenko <digdog649@gmail.com>
2026-04-23 13:15:49 +0200

    fix file-path config.xml

--- src/main/java/ua/hrinchenko/Main.java
+++ src/main/java/ua/hrinchenko/Main.java
@@ -6,9 +6,8 @@ import ua.hrinchenko.map.GameField;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\uD83C\uDF31 Preparation of the island...");
-        // 1. Завантажуємо налаштування (якщо ти використовуєш XML парсер, залиш цей рядок)
-        // Якщо використовуєш звичайний клас Config або Enum, цей рядок не потрібен.
-        Config.load("C:\\Users\\digdo\\Documents\\Java Projects\\TranningProject\\src\\main\\resources\\config.xml");
+        // 1. Завантажуємо налаштування
+        Config.load("C:\\Users\\digdo\\Documents\\Java Projects\\yevhen_hrinchenko_island\\src\\main\\resources\\config.xml");
 
         // 2. Створюємо карту острова (беремо розміри з конфігурації)
         System.out.println("🌍 Creating an island " + Config.ISLAND_WIDTH + "x" + Config.ISLAND_HEIGHT + "...");
